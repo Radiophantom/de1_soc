83a66839950c5f3efc5ac1e17ddab319d0d7ef79
Sergei Krivchenko <s.krivchenko@metrotek.ru>
2025-03-17 18:40:33 +0300

    Successfully appeared in Linux.

--- top.sv
+++ top.sv
@@ -49,9 +49,28 @@ module top(
       output             FPGA_I2C_SCLK,
       inout              FPGA_I2C_SDAT,
 
+      ///////// PCI  /////////
+      input              PCI_RSTn,
+      input              PCI_CLK,
+      input              PCI_IDSEL,
+      input              PCI_LOCKn,
+
+      inout     [31:0]   PCI_AD,
+      inout              PCI_PAR,
+      inout     [3:0]    PCI_CBEn,
+      inout              PCI_FRAMEn,
+      inout              PCI_IRDYn,
+      inout              PCI_TRDYn,
+      inout              PCI_STOPn,
+      inout              PCI_DEVSELn,
+      inout              PCI_PERRn,
+
+      output             PCI_SERRn,  // Open-drain
+      output             PCI_INTAn, // Open-drain
+
       ///////// GPIO /////////
-      inout      [35:0]  GPIO_0,
-      inout      [35:0]  GPIO_1,
+      //inout      [35:0]  GPIO_0,
+      //inout      [35:0]  GPIO_1,
 
       ///////// HEX0 /////////
       output      [6:0]  HEX0,
@@ -196,18 +215,8 @@ assign HEX5 = '0;
 logic pci_rst;
 logic pci_clk;
 
-assign pci_rst = !GPIO_0[0]; // NOTE: RESETn is inverted signal
-assign pci_clk =  GPIO_0[2];
-
-logic             frame_i;
-logic [3:0][7:0]  ad_i;
-logic [3:0]       cbe_i;
-logic             par_i;
-logic             irdy_i;
-logic             trdy_i;
-logic             stop_i;
-logic             devsel_i;
-logic             idsel_i;
+assign pci_rst = !PCI_RSTn; // NOTE: RESETn is inverted signal
+assign pci_clk =  PCI_CLK;
 
 logic             frame_o;
 logic [3:0][7:0]  ad_o;
@@ -217,6 +226,9 @@ logic             irdy_o;
 logic             trdy_o;
 logic             stop_o;
 logic             devsel_o;
+logic             perr_o  = 1'b1;
+logic             serr_o  = 1'b0;
+logic             int_o   = 1'b0;
 
 logic             frame_o_en;
 logic             ad_o_en;
@@ -226,31 +238,21 @@ logic             irdy_o_en;
 logic             trdy_o_en;
 logic             stop_o_en;
 logic             devsel_o_en;
+logic             perr_o_en = 1'b0;
 
-assign frame_i  = GPIO_1[ 5];
-assign ad_i[3] = {GPIO_0[30],GPIO_0[28],GPIO_0[26],GPIO_0[24],GPIO_0[22],GPIO_0[20],GPIO_0[18],GPIO_0[16]};
-assign ad_i[2] = {GPIO_0[14],GPIO_0[12],GPIO_0[10],GPIO_0[ 8],GPIO_0[ 6],GPIO_0[ 4],GPIO_0[32],GPIO_0[35]};
-assign ad_i[1] = {GPIO_0[33],GPIO_0[31],GPIO_0[29],GPIO_0[27],GPIO_0[25],GPIO_0[23],GPIO_0[21],GPIO_0[19]};
-assign ad_i[0] = {GPIO_0[17],GPIO_0[15],GPIO_0[13],GPIO_0[11],GPIO_0[ 9],GPIO_0[ 7],GPIO_0[ 5],GPIO_0[ 3]};
-assign cbe_i   = {GPIO_1[6],GPIO_0[4],GPIO_0[2],GPIO_0[0]};
-assign par_i    = GPIO_1[ 1];
-assign irdy_i   = GPIO_1[ 7];
-assign trdy_i   = GPIO_1[ 9];
-assign stop_i   = GPIO_1[11];
-assign devsel_i = GPIO_1[13];
-assign idsel_i  = GPIO_1[ 8];
+assign PCI_FRAMEn   = frame_o_en  ? frame_o   : 'Z;
+assign PCI_AD       = ad_o_en     ? ad_o      : 'Z;
+assign PCI_CBEn     = cbe_o_en    ? cbe_o     : 'Z;
+assign PCI_PAR      = par_o_en    ? par_o     : 'Z;
+assign PCI_IRDYn    = irdy_o_en   ? irdy_o    : 'Z;
+assign PCI_TRDYn    = trdy_o_en   ? trdy_o    : 'Z;
+assign PCI_STOPn    = stop_o_en   ? stop_o    : 'Z;
+assign PCI_DEVSELn  = devsel_o_en ? devsel_o  : 'Z;
+assign PCI_PERRn    = perr_o_en   ? perr_o    : 'Z;
 
-assign  GPIO_1[ 5] = frame_o_en ? frame_o : 'Z;
-assign  {GPIO_0[30],GPIO_0[28],GPIO_0[26],GPIO_0[24],GPIO_0[22],GPIO_0[20],GPIO_0[18],GPIO_0[16]} = ad_o_en ? ad_o[3] : 'Z;
-assign  {GPIO_0[14],GPIO_0[12],GPIO_0[10],GPIO_0[ 8],GPIO_0[ 6],GPIO_0[ 4],GPIO_0[32],GPIO_0[35]} = ad_o_en ? ad_o[2] : 'Z;
-assign  {GPIO_0[33],GPIO_0[31],GPIO_0[29],GPIO_0[27],GPIO_0[25],GPIO_0[23],GPIO_0[21],GPIO_0[19]} = ad_o_en ? ad_o[1] : 'Z;
-assign  {GPIO_0[17],GPIO_0[15],GPIO_0[13],GPIO_0[11],GPIO_0[ 9],GPIO_0[ 7],GPIO_0[ 5],GPIO_0[ 3]} = ad_o_en ? ad_o[0] : 'Z;
-assign  {GPIO_1[6],GPIO_0[4],GPIO_0[2],GPIO_0[0]} = cbe_o_en ? cbe_o : 'Z;
-assign  GPIO_1[ 1] = par_o_en     ? par_o     : 'Z;
-assign  GPIO_1[ 7] = irdy_o_en    ? irdy_o    : 'Z;
-assign  GPIO_1[ 9] = trdy_o_en    ? trdy_o    : 'Z;
-assign  GPIO_1[11] = stop_o_en    ? stop_o    : 'Z;
-assign  GPIO_1[13] = devsel_o_en  ? devsel_o  : 'Z;
+// Not used signals now
+assign PCI_SERRn  = serr_o ? 1'b0 : 'Z;
+assign PCI_INTAn  = int_o  ? 1'b0 : 'Z;
 
 avalon_mm_if #(32,32) mem_if (pci_clk);
 
@@ -258,15 +260,15 @@ pci_core_top pci_core_top_inst (
   .clk_i          ( pci_clk     ),
   .rst_i          ( pci_rst     ),
 
-  .FRAMEn_in      ( frame_i     ),
-  .AD_in          ( ad_i        ),
-  .PAR_in         ( par_i       ),
-  .CBEn_in        ( cbe_i       ),
-  .IRDYn_in       ( irdy_i      ),
-  .TRDYn_in       ( trdy_i      ),
-  .DEVSELn_in     ( devsel_i    ),
-  .STOPn_in       ( stop_i      ),
-  .IDSEL_in       ( idsel_i     ),
+  .FRAMEn_in      ( PCI_FRAMEn  ),
+  .AD_in          ( PCI_AD      ),
+  .PAR_in         ( PCI_PAR     ),
+  .CBEn_in        ( PCI_CBEn    ),
+  .IRDYn_in       ( PCI_IRDYn   ),
+  .TRDYn_in       ( PCI_TRDYn   ),
+  .DEVSELn_in     ( PCI_DEVSELn ),
+  .STOPn_in       ( PCI_STOPn   ),
+  .IDSEL_in       ( PCI_IDSEL   ),
 
   .FRAMEn_out     ( frame_o     ),
   .AD_out         ( ad_o        ),
